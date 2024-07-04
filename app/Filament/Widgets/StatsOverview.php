e0f7f7ca25acfe51154a05a376566655dade3c8b
unknown <okakzai@gmail.com>
2024-07-05 06:47:39 +0700

    Edit file 'app/Filament/Widgets/StatsOverview.php'

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -4,29 +4,18 @@ namespace App\Filament\Widgets;
 
 
 use App\Models\Transaction;
-use Illuminate\Support\Carbon;
 use Filament\Widgets\StatsOverviewWidget\Stat;
-use Filament\Widgets\Concerns\InteractsWithPageFilters;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 
 class StatsOverview extends BaseWidget
 {
-    use InteractsWithPageFilters;
     protected function getStats(): array
     {
-        $startDate = ! is_null($this->filters['startDate'] ?? null) ?
-            Carbon::parse($this->filters['startDate']) :
-            null;
-
-        $endDate = ! is_null($this->filters['endDate'] ?? null) ?
-            Carbon::parse($this->filters['endDate']) :
-            now();
-
         $pemasukan = Transaction::incomes()
-            ->whereBetween('tanggal',[$startDate,$endDate])
+            ->get()
             ->sum('amount');
         $pengeluaran = Transaction::outcomes()
-            ->whereBetween('tanggal',[$startDate,$endDate])
+            ->get()
             ->sum('amount');
         return [
             Stat::make('Total Pemasukan', $pemasukan),
